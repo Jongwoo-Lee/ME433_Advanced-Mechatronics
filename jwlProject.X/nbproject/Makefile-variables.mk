e9a9f8169f10383f7db382ea9467c311e32c4640
jongwooME <jongwoolee2014@u.northwestern.edu>
2015-04-02 09:11:44 -0500

    HW1
    Green LED  - Potentiometer
    Yellow LED - USER button

---
+++ jwlProject.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=jwlProject.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/jwlProject.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=jwlproject.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/jwlproject.x.tar
